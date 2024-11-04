bb4118b4584dce974cbcea8a5d30523789215e9b
tomas12py <tomas.martinez@polygon.us>
2024-11-03 21:23:25 -0500

    The navbar component was created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import logo from '../assets/logo.png'
+import { useState } from 'react'
+import '../styles/app.css'
+
+
+export const Navbar = ({routes = []}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="navContainer">
+        <div className="logo"><img className = "logo-img"src={logo} alt="" /></div>
+        
+        <button 
+          className="hamburger"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Toggle menu"
+        >
+          ☰
+        </button>
+
+        <div className={`navLinks ${isOpen ? 'open' : ''}`}>
+        {routes.map((route,index) => (
+            <a
+              key={index}
+              href="#"
+              className="navLink"
+            >
+              {route}
+            </a>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+
