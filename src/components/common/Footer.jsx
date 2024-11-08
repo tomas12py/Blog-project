9224294fd7858a60ed4d77968eeefba54a37b6b8
tomas12py <tomas.martinez@polygon.us>
2024-11-07 21:02:47 -0500

    The footer component and its styles have been added.

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,46 @@
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { MdOutlineMail } from "react-icons/md";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+export const Footer = () => {
+  return (
+<footer className="footer">
+    <h3 className="footer-phrase">Redes sociales y contactos</h3>
+    <div className="social-icons">
+      <a
+        href="https://www.facebook.com"
+        target="_blank"
+        className="social-icon"
+      >
+       <FaFacebook/>
+      </a>
+      <a href="https://www.twitter.com" target="_blank" className="social-icon">
+      <FaSquareXTwitter/>
+      </a>
+      <a
+        href="https://www.instagram.com"
+        target="_blank"
+        className="social-icon"
+      >
+       <FaInstagram/>
+      </a>
+      <a
+        href="mailto:tomi12mc@gmail.com"
+        target="_blank"
+        className="social-icon"
+      >
+       <MdOutlineMail/>
+      </a>
+      <a
+        href="https://wa.link/6oibtt"
+        target="_blank"
+        className="social-icon"
+      >
+       <FaWhatsapp/>
+      </a>
+    </div>
+  </footer>
+
+  )
+}
