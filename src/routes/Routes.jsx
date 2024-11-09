459e1bffb49001e6c3cbf3243f6330def31bb57c
tomas12py <tomas.martinez@polygon.us>
2024-11-08 21:55:08 -0500

    A new path has been added to create user

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,11 @@
-import {Routes,Route} from 'react-router-dom'
-import { Navbar } from '../components/common/Navbar'
 import { LandingPage } from '../components/pages/LandingPage'
+import { FormUser } from '../components/pages/FormUser'
+import {Routes,Route} from 'react-router-dom'
 export const ProjectRoutes = () => {
   return (
        <Routes>
         <Route path = "/" element = {<LandingPage/>}></Route>
+        <Route path = "/create-user" element = {<FormUser/>}></Route>
         </Routes> 
   ) 
 }
