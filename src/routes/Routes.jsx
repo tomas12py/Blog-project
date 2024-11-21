b9edb1ccee6f91f131d53e6ce9318ea617db338c
tomas12py <tomas.martinez@polygon.us>
2024-11-20 23:26:03 -0500

    The view for login was created

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { LandingPage } from '../components/pages/LandingPage'
 import { FormUser } from '../components/pages/FormUser'
+import {Login} from '../components/pages/Login'
 import {Routes,Route} from 'react-router-dom'
 export const ProjectRoutes = () => {
   return (
        <Routes>
         <Route path = "/" element = {<LandingPage/>}></Route>
         <Route path = "/create-user" element = {<FormUser/>}></Route>
+        <Route path = "/login" element = {<Login/>}></Route>
         </Routes> 
   ) 
 }
