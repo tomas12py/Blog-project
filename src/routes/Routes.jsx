1148ff179c7f7fb488bdb2af2db2172c68da8f29
tomas12py <tomas.martinez@polygon.us>
2024-11-23 07:25:54 -0500

    Settings for deploy was implemented

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,9 +3,10 @@ import { LandingPage } from '../components/pages/LandingPage'
 import { FormPublication } from '../components/pages/FormPublication'
 import { FormUser } from '../components/pages/FormUser'
 import {Login} from '../components/pages/Login'
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route,Router} from 'react-router-dom'
 export const ProjectRoutes = () => {
   return (
+      <Router basename='/Blog-project'>
        <Routes>
         <Route path = "/" element = {<LandingPage/>}></Route>
         <Route path = "/create-user" element = {<FormUser/>}></Route>
@@ -13,5 +14,6 @@ export const ProjectRoutes = () => {
         <Route path='/publications' element = {<UserPublications/>}></Route>
         <Route path='create-publication' element ={<FormPublication/>}></Route>
         </Routes> 
+      </Router>
   ) 
 }
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://tomas12py.github.io/Blog-project",
   plugins: [react()],
 
 })
