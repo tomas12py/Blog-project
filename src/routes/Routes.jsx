50261ef9d3ce5a94a21f975dabddcb1340efcb0f
tomas12py <tomas.martinez@polygon.us>
2025-11-19 15:37:08 -0500

    Commits for deployment were eliminated

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,10 +3,9 @@ import { LandingPage } from '../components/pages/LandingPage'
 import { FormPublication } from '../components/pages/FormPublication'
 import { FormUser } from '../components/pages/FormUser'
 import {Login} from '../components/pages/Login'
-import {Routes,Route,Router} from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
 export const ProjectRoutes = () => {
   return (
-      <Router basename='/Blog-project'>
        <Routes>
         <Route path = "/" element = {<LandingPage/>}></Route>
         <Route path = "/create-user" element = {<FormUser/>}></Route>
@@ -14,6 +13,5 @@ export const ProjectRoutes = () => {
         <Route path='/publications' element = {<UserPublications/>}></Route>
         <Route path='create-publication' element ={<FormPublication/>}></Route>
         </Routes> 
-      </Router>
   ) 
 }
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://tomas12py.github.io/Blog-project",
   plugins: [react()],
 
 })
